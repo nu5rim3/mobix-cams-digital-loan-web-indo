67f1a0688216013a33a235e9e5b836a4dba1c2e4
Damith Alwis <damithal@lolctech.com>
2023-10-29 10:24:52 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -8,8 +8,8 @@ import { BrowserRouter } from "react-router-dom";
 const authConfig: TAuthConfig = {
   clientId: import.meta.env.VITE_CLIENT_ID,
   authorizationEndpoint: import.meta.env.VITE_AUTHORIZATION_ENDPOINT,
-  tokenEndpoint: 'https://indodigitalmedev.lolc.com/indo-digital-loan/oauth2/token',
-  redirectUri: 'https://indodevlb1/indo-digital-loan/redirect1',
+  tokenEndpoint: 'https://indodevlb1/indo-digital-loan/oauth2/token',
+  redirectUri: 'https://indodevlb1/indo-digital-loan/redirect',
   scope: 'someScope openid',
   onRefreshTokenExpire: (event: TRefreshTokenExpiredEvent) => window.confirm('Session expired. Refresh page to continue using the site?') && event.login(),
 }
