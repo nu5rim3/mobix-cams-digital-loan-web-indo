0ed2d41cc45789bbac3632271c8244290530819b
Damith Alwis <damithal@lolctech.com>
2023-10-29 09:57:44 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
 
   return {
-    base: "/indo-digital-loan",
+    base: "/",
     server: {
       cors: {
         origin : true
