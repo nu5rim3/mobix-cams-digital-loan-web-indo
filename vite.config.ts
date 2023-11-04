70910a5fbce419e24bfcfc5affcdfc0900063637
Damith <damithal@lolctech.com>
2023-11-04 21:56:44 +0530

    origin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig, loadEnv } from 'vite'
 import http from "https";
 import react from '@vitejs/plugin-react-swc'
+import dns from 'dns'
 
+dns.setDefaultResultOrder('verbatim')
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
 
