e5eb0feedb9b256479e10e12b4904049f4170729
Damith <damithal@lolctech.com>
2023-11-04 20:13:34 +0530

    https

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, loadEnv } from 'vite'
+import http from "https";
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
@@ -10,20 +11,26 @@ export default defineConfig(({ command, mode }) => {
     base: "/indo-digital-loan",
     server: {
       cors: {
-        origin : true
+        origin: true
       },
       proxy: {
         '/indo-digital-loan/oauth2': {
           target: env.VITE_AUTHORIZATION_SERVER,
           changeOrigin: true,
           secure: true,
-          rewrite: (path) => path.replace(/^\/indo-digital-loan/, ''),
+          agent: new http.Agent(),
+          rewrite: (path) => {
+            return path.replace(/^\/indo-digital-loan/, '')
+          },
         },
         '/indo-digital-loan/token': {
           target: env.VITE_INDO_BASE_URL,
           changeOrigin: true,
           secure: false,
-          rewrite: (path) => path.replace(/^\/indo-digital-loan/, ''),
+          agent: new http.Agent(),
+          rewrite: (path) => {
+            return path.replace(/^\/indo-digital-loan/, '')
+          }
         },
         '/indo-digital-loan/mobixCamsCommon': {
           target: env.VITE_INDO_BASE_URL,
@@ -33,32 +40,32 @@ export default defineConfig(({ command, mode }) => {
             return path.replace(/^\/indo-digital-loan/, '')
           },
         },
-        '/indo-digital-loan/mobixCamsCredit':  {
+        '/indo-digital-loan/mobixCamsCredit': {
           target: env.VITE_INDO_BASE_URL,
           changeOrigin: true,
           secure: false,
           rewrite: (path) => path.replace(/^\/indo-digital-loan/, ''),
         },
-        '/indo-digital-loan/mobixCamsLoan':  {
+        '/indo-digital-loan/mobixCamsLoan': {
           target: env.VITE_INDO_BASE_URL,
           changeOrigin: true,
           secure: false,
           rewrite: (path) => path.replace(/^\/indo-digital-loan/, ''),
         },
-        '/indo-digital-loan/mobixCamsStakeholder':  {
+        '/indo-digital-loan/mobixCamsStakeholder': {
           target: env.VITE_INDO_BASE_URL,
           changeOrigin: true,
           secure: false,
           rewrite: (path) => path.replace(/^\/indo-digital-loan/, ''),
         },
-        '/indo-digital-loan/mobixCamsDocument':  {
+        '/indo-digital-loan/mobixCamsDocument': {
           target: env.VITE_INDO_BASE_URL,
           changeOrigin: true,
           secure: false,
           rewrite: (path) => path.replace(/^\/indo-digital-loan/, ''),
         }
         ,
-        '/indo-digital-loan/mobixCamsApproval':  {
+        '/indo-digital-loan/mobixCamsApproval': {
           target: env.VITE_INDO_BASE_URL,
           changeOrigin: true,
           secure: false,
