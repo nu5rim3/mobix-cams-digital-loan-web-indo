59cf57171cb546448e168da94cc12c592c0bbfc3
shibly-r <shiblyr@lolctech.com>
2023-12-13 09:48:21 +0530

    set headers

--- src/services/config.ts
+++ src/services/config.ts
@@ -16,6 +16,10 @@ export const setAxiosToken = async (token:string, idx: any) => {
         axiosFileInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
         axiosInstance.defaults.headers.common['X-Auth-Token'] = btoa(idx);
         axiosFileInstance.defaults.headers.common['X-Auth-Token'] = btoa(idx);
+        axiosInstance.defaults.headers.common['Content-Security-Policy'] = "default-src 'self' http: https: data: blob: 'unsafe-inline'";
+        axiosInstance.defaults.headers.common['X-Frame-Options'] = "SAMEORIGIN";
+        axiosInstance.defaults.headers.common['Set-Cookie'] = "Path=/; HttpOnly; Secure";
+        axiosInstance.defaults.headers.common['Strict-Transport-Security'] = "max-age=31536000; includeSubDomains";
     }
   };
 
