0977a31618f86ad4577240bde0e7b574931d4bff
Damith <damithal@lolctech.com>
2024-05-07 11:39:24 +0530

    front cam

--- src/components/Camera/CapturePhoto.tsx
+++ src/components/Camera/CapturePhoto.tsx
@@ -1,44 +1,50 @@
 import { Button, Modal } from 'antd';
 import * as React from 'react';
 import Webcam from "react-webcam";
-import {CameraOutlined} from '@ant-design/icons'
+import { CameraOutlined } from '@ant-design/icons'
 
 const WebcamCapture = ({
   open,
   setOpen,
   onCapture
-}:{
-  open: boolean,
-  setOpen: Function,
-  onCapture: Function
-}) => {
+}: {
+    open: boolean,
+    setOpen: Function,
+    onCapture: Function
+  }) => {
 
-    const webcamRef:any = React.useRef(null);
-    const [imgSrc, setImgSrc] = React.useState(null);
-  
-    const capture = React.useCallback(() => {
-      const imageSrc = webcamRef?.current?.getScreenshot();
-      console.log("cure", webcamRef?.current)
-      const imageSrc1 = webcamRef?.current
+  const webcamRef: any = React.useRef(null);
+  const [imgSrc, setImgSrc] = React.useState(null);
+
+  const capture = React.useCallback(() => {
+    const imageSrc = webcamRef ?.current ?.getScreenshot();
+    console.log("cure", webcamRef ?.current)
+    const imageSrc1 = webcamRef ?.current
       // setImgSrc(imageSrc);
       onCapture(imageSrc)
-      setOpen(false)
-    }, [webcamRef, setImgSrc]);
-  
-    return (
-      <Modal
-        title="Capture Photo"
-        open={open}
-        onCancel={() => setOpen(false)}
-        footer={false}
-        width={700}
-        centered
-      >
+    setOpen(false)
+  }, [webcamRef, setImgSrc]);
+
+  const videoConstraints = {
+    width: { min: 480 },
+    height: { min: 720 },
+    facingMode: { exact: "environment" }
+  };
+
+  return (
+    <Modal
+      title="Capture Photo"
+      open={open}
+      onCancel={() => setOpen(false)}
+      footer={false}
+      width={700}
+      centered
+    >
       <>
         <Webcam
           audio={false}
           ref={webcamRef}
-          screenshotFormat="image/jpeg"
+          screenshotFormat="image/jpeg" videoConstraints={videoConstraints}
         />
         <Button icon={<CameraOutlined />} className='w-full mt-3' onClick={capture} type='primary'>CAPTURE PHOTO</Button>
         {/* {imgSrc && (
@@ -48,7 +54,7 @@ const WebcamCapture = ({
         )} */}
       </>
     </Modal>
-    );
-  };
+  );
+};
 
 export default WebcamCapture
