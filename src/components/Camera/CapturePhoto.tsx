d759f3541a456e734efcaebff6f34e43914499c7
Damith <damithal@lolctech.com>
2024-05-20 14:05:27 +0530

    mobile back camera

--- src/components/Camera/CapturePhoto.tsx
+++ src/components/Camera/CapturePhoto.tsx
@@ -1,35 +1,50 @@
 import { Button, Modal } from 'antd';
 import * as React from 'react';
 import Webcam from "react-webcam";
-import { CameraOutlined } from '@ant-design/icons'
+import { CameraOutlined, RotateRightOutlined } from '@ant-design/icons'
+
+interface VideoConstraints {
+  width: { min: number };
+  height: { min: number };
+  facingMode: string | { exact: string };
+}
 
 const WebcamCapture = ({
   open,
   setOpen,
   onCapture
 }: {
-    open: boolean,
-    setOpen: Function,
-    onCapture: Function
-  }) => {
-
+  open: boolean,
+  setOpen: Function,
+  onCapture: Function
+}) => {
+  const [toggle, setToggle] = React.useState<boolean>(false)
   const webcamRef: any = React.useRef(null);
-  const [imgSrc, setImgSrc] = React.useState(null);
+  const [videoConstraints, setVideoConstraints] = React.useState<VideoConstraints>({
+    width: { min: 380 },
+    height: { min: 400 },
+    facingMode: "user",
+  });
+
+  const toggleCamera = () => {
+    setVideoConstraints((prevConstraints) => ({
+      ...prevConstraints,
+      facingMode: toggle ? { exact: "environment" } : "user",
+    }));
+  };
 
   const capture = React.useCallback(() => {
-    const imageSrc = webcamRef ?.current ?.getScreenshot();
-    console.log("cure", webcamRef ?.current)
-    const imageSrc1 = webcamRef ?.current
-      // setImgSrc(imageSrc);
-      onCapture(imageSrc)
+    const imageSrc = webcamRef?.current?.getScreenshot();
+    onCapture(imageSrc)
     setOpen(false)
-  }, [webcamRef, setImgSrc]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [webcamRef]);
 
-  const videoConstraints = {
-    width: { min: 480 },
-    height: { min: 720 },
-    facingMode: "user",
-  };
+  React.useEffect(() => {
+    toggleCamera()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [toggle])
+
 
   return (
     <Modal
@@ -44,14 +59,13 @@ const WebcamCapture = ({
         <Webcam
           audio={false}
           ref={webcamRef}
-          screenshotFormat="image/jpeg" 
+          screenshotFormat="image/jpeg"
+          videoConstraints={videoConstraints}
         />
-        <Button icon={<CameraOutlined />} className='w-full mt-3' onClick={capture} type='primary'>CAPTURE PHOTO</Button>
-        {/* {imgSrc && (
-          <img
-            src={imgSrc}
-          />
-        )} */}
+        <div className='flex justify-between mt-3'>
+          <Button icon={<RotateRightOutlined />} className='sm:hidden block w-1/3 mr-2' onClick={() => setToggle(!toggle)} type='default'>TOGGLE</Button>
+          <Button icon={<CameraOutlined />} className='w-2/3 sm:w-full' onClick={capture} type='primary'>CAPTURE PHOTO</Button>
+        </div>
       </>
     </Modal>
   );
