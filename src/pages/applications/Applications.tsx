9b27e1cd1731d0f66651f4c6d4cf487b016e5a87
nu5rim3 <nusrisamath@gmail.com>
2024-08-26 16:00:17 +0530

    fix: search date range issue

--- src/pages/applications/Applications.tsx
+++ src/pages/applications/Applications.tsx
@@ -2,7 +2,7 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ColumnsType } from 'antd/es/table';
-import { DatePicker, Input, Select, Space, Tag, theme } from 'antd';
+import { DatePicker, Input, Select, Space, Tag } from 'antd';
 import BreadCrumbContainer from '../../components/Containers/BreadCrumbContainer';
 import Title from '../../components/Typography/Tytle';
 import ContentContainer from '../../components/Containers/ContentContainer';
@@ -18,16 +18,15 @@ export interface IApplicationsProps {
 
 }
 
+const { RangePicker } = DatePicker;
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function Applications(_props: IApplicationsProps) {
   const navigate = useNavigate();
   const [searchStatus, setSearchStatus] = useState<string>('APPROVAL_PENDING')
   const [searchAppraisal, setSearchAppraisal] = useState<string | null>(null)
-  const [fromDateFilter, setFromDateFilter] = useState(null);
-  const [toDateFilter, setToDateFilter] = useState(null);
-  const {
-    token: { colorTextHeading },
-  } = theme.useToken();
+  const [fromDateFilter, setFromDateFilter] = useState<string>('');
+  const [toDateFilter, setToDateFilter] = useState<string>('');
 
   const {
     applications
@@ -42,48 +41,29 @@ export default function Applications(_props: IApplicationsProps) {
   const [pageSize, setPageSize] = useState(7);
 
   useEffect(() => {
-    if (searchAppraisal === null) {
-      actions.getAllApplications({
-        role: selectedRole,
-        status: searchStatus,
-        branch: userData?.data?.branches[0]?.code,
-        page: currentPage - 1,
-        size: pageSize
-      })
-    }
-    else if (searchAppraisal !== null) {
-      actions.getAllApplications({
-        role: selectedRole,
-        appraisalId: searchAppraisal,
-        fromDate: fromDateFilter,
-        toDate: toDateFilter,
-        status: searchStatus,
-        branch: userData?.data?.branches[0]?.code,
-        page: currentPage - 1,
-        size: pageSize
-      })
-    }
+
+    actions.getAllApplications({
+      role: selectedRole,
+      appraisalId: searchAppraisal === null ? '' : searchAppraisal,
+      fromDate: fromDateFilter === '' ? '' : fromDateFilter,
+      toDate: toDateFilter === '' ? '' : toDateFilter,
+      status: searchStatus,
+      branch: userData?.data?.branches[0]?.code,
+      page: currentPage - 1,
+      size: pageSize
+    })
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [pageSize, currentPage, searchStatus, searchAppraisal, toDateFilter, fromDateFilter])
 
-  const handleFromDateFilterChange = (date: any) => {
-    if (date) {
-      setFromDateFilter(date.format('YYYY-MM-DD'));
-    } else {
-      setFromDateFilter(null);
-    }
-  };
+  useEffect(() => {
+    setCurrentPage(1)
+    setPageSize(7)
+  }, [searchStatus])
 
-  const handleToDateFilterChange = (date: any) => {
-    if (date) {
-      setToDateFilter(date.format('YYYY-MM-DD'));
-    } else {
-      setToDateFilter(null)
-    }
-  };
 
   const searchData = () => {
     setSearchStatus(searchStatus)
+
     actions.getAllApplications({
       role: selectedRole,
       appraisalId: searchAppraisal,
@@ -111,6 +91,8 @@ export default function Applications(_props: IApplicationsProps) {
             return <div className='flex justify-between'><Tag color='red' key={status}>{idx}</Tag><CopyOutlined onClick={() => copyToClipborad(idx)} /></div>;
           case "C":
             return <div className='flex justify-between'><Tag color='green' key={status}>{idx}</Tag><CopyOutlined onClick={() => copyToClipborad(idx)} /></div>;
+          case "A":
+            return <div className='flex justify-between'><Tag color='yellow' key={status}>{idx}</Tag><CopyOutlined onClick={() => copyToClipborad(idx)} /></div>;
           case "AP":
             return <div className='flex justify-between'><Tag color='blue' key={status}>{idx}</Tag><CopyOutlined onClick={() => copyToClipborad(idx)} /></div>;
           default:
@@ -187,6 +169,17 @@ export default function Applications(_props: IApplicationsProps) {
     setSearchStatus(value)
 
   }
+
+
+  const onResetSearch = () => {
+    setSearchAppraisal('')
+    setFromDateFilter('')
+    setToDateFilter('')
+    setSearchStatus('APPROVAL_PENDING')
+    setCurrentPage(1)
+    setPageSize(7)
+  }
+
   return (
     <div>
       <BreadCrumbContainer>
@@ -242,12 +235,23 @@ export default function Applications(_props: IApplicationsProps) {
             className='mr-2 w-2/6'
             allowClear
             onChange={(e) => setSearchAppraisal(e.target.value)}
+            value={searchAppraisal || ''}
           />
-          <DatePicker size={'middle'} onChange={handleFromDateFilterChange} />
+          {/* <DatePicker size={'middle'} onChange={handleFromDateFilterChange} />
           <div className='m-2 font-bold' style={{ color: colorTextHeading }}>
             To
           </div>
-          <DatePicker size={'middle'} onChange={handleToDateFilterChange} />
+          <DatePicker size={'middle'} onChange={handleToDateFilterChange} /> */}
+          <RangePicker
+            size='middle'
+            onCalendarChange={(value: any) => {
+              if (value && value?.length !== 0) {
+                setToDateFilter(value[1].format('YYYY-MM-DD'))
+                setFromDateFilter(value[0].format('YYYY-MM-DD'))
+              }
+            }}
+            allowClear
+          />
           <ButtonContainer
             type='primary'
             label='Search'
@@ -255,6 +259,14 @@ export default function Applications(_props: IApplicationsProps) {
             className='ml-3'
             onClick={() => {
               searchData()
+            }} />
+          <ButtonContainer
+            type='default'
+            label='Reset'
+            size='middle'
+            className='ml-3'
+            onClick={() => {
+              onResetSearch()
             }} />
         </div>
 
